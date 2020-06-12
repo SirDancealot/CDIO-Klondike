558bec150936075d24854486fafb4710704e1a04
SirDancealot <mj@lpj-software.dk>
2020-06-12 14:23:56 +0200

    finished encoding of a game and added a note for how cards are encoded

--- Object_detection/encode.py
+++ Object_detection/encode.py
@@ -2,14 +2,15 @@ from enum import IntEnum
 
 
 class Suit(IntEnum):
+    HIDDEN = 0
     HEARTS = 1
     SPADES = 2
     DIAMONDS = 3
     CLUBS = 4
-    HIDDEN = 15
 
 
 class Value(IntEnum):
+    HIDDEN = 0
     ACE = 1
     TWO = 2
     THREE = 3
@@ -23,7 +24,6 @@ class Value(IntEnum):
     JACK = 11
     QUEEN = 12
     KING = 13
-    HIDDEN = 15
 
 
 class Card:
@@ -36,8 +36,8 @@ class Card:
 
 
 class GameState:
-    gameCards = [[], [], [], [], [], [], []]
-    finalCards = [[], [], [], []]
+    gameCards = [[Card(Suit.DIAMONDS, Value.KING), Card(Suit.SPADES, Value.ACE)], [], [], [], [], [], []]
+    finalCards = [None, Card(Suit.DIAMONDS, Value.ACE), None, None]
     hiddenStock = None
     shownStock = None
 
@@ -45,32 +45,54 @@ class GameState:
 HIDDEN = Card(Suit.HIDDEN, Value.HIDDEN)
 testCard = Card(Suit.DIAMONDS, Value.ACE)
 
+
 def encode_card(card):
+    if card is None:
+        return chr(0b0)
     num = (card.value & 0b1111) << 4
     suit = card.suit & 0b1111
     return chr(num + suit)
 
 
 def encode_game(gameState):
-    if gameState is not GameState:
-       raise Exception("gameState variable is not class GameState")
+    if gameState.__class__ is not GameState:
+        raise Exception("gameState variable is not class GameState")
 
+    return_str = ""
     gCardsLen = []
-    fCardsLen = []
 
     for i in range(len(gameState.gameCards)):
-        if (gameState.gameCards[i] is None) || (len(gameState.gameCards[i]) == 0):
-            gCardsLen.append(255)
+        if (gameState.gameCards[i] is None) or (len(gameState.gameCards[i]) == 0):
+            gCardsLen.append(0)
         else:
             gCardsLen.append(len(gameState.gameCards[i]))
 
-    for i in range(len(gameState.finalCards)):
-        if (gameState.finalCards[i] is None) || (len(gameState.gameCards[i]) == 0):
-            fCardsLen.append(0)
-        else:
-            fCardsLen.append(len(gameState.finalCards[i]))
-    return ""
+    for i in range(len(gCardsLen)):
+        return_str += chr(gCardsLen[i])
+        for j in range(gCardsLen[i]):
+            return_str += encode_card(gameState.gameCards[i][j])
+
+    for i in range(4):
+        return_str += encode_card(gameState.finalCards[i])
+
+    if gameState.hiddenStock is None:
+        return_str += chr(0xFF)
+    else:
+        return_str += encode_card(gameState.hiddenStock)
+
+    if gameState.shownStock is None:
+        return_str += chr(0xFF)
+    else:
+        return_str += encode_card(gameState.shownStock)
+
+    return return_str
+
 
 char = encode_card(HIDDEN)
+test = encode_game(GameState())
 print("Encoded card: '" + char + "'")
 print("Encoded card as byte: '" + bin(ord(char)) + "'")
+print("test: '" + test + "'")
+
+for c in test:
+    print(bin(ord(c)))
